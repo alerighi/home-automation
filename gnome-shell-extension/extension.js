a0e6b4d196dc9b691b82b394fc22fa487e97540c
alerighi <alessandro.righi@iotinga.it>
2021-11-19 22:01:14 +0100

    add gnome shell extension code

---
+++ gnome-shell-extension/extension.js
@@ -0,0 +1,132 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const { GObject, St } = imports.gi;
+
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Soup = imports.gi.Soup;
+const Mainloop = imports.mainloop;
+
+const POLL_INTERVAL_SEC = 20
+const BASE_URL = 'http://192.168.1.200:8080/light'
+
+function setLight(light, value) {
+    const soupSyncSession = new Soup.SessionSync();
+    const message = Soup.Message.new('GET', BASE_URL + '?light=' + light + '&on=' + value);
+    const responseCode = soupSyncSession.send_message(message);
+
+    if (responseCode == 200) {
+        return JSON.parse(message.response_body.data)
+    }
+}
+
+
+function getLightState() {
+    const soupSyncSession = new Soup.SessionSync();
+    const message = Soup.Message.new('GET', BASE_URL);
+    const responseCode = soupSyncSession.send_message(message);
+    
+    if (responseCode == 200) {
+        return JSON.parse(message.response_body.data)
+    }
+}
+
+
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, 'Home automation');        
+
+        this.add_child(new St.Icon({
+            icon_name: 'dialog-information-symbolic',
+            style_class: 'system-status-icon',
+        }));
+
+        // create switches
+        this.ufficio = new PopupMenu.PopupSwitchMenuItem('Ufficio', false);
+        this.scale = new PopupMenu.PopupSwitchMenuItem('Scale', false);
+        this.bagno = new PopupMenu.PopupSwitchMenuItem('Bagno', false);
+        this.camera = new PopupMenu.PopupSwitchMenuItem('Camera', false);
+        this.armadio = new PopupMenu.PopupSwitchMenuItem('Armadio', false);
+        
+        // connect signals
+        this.ufficio.connect('activate', (params) => this.setLightState(setLight('ufficio', params.state))) 
+        this.scale.connect('activate', (params) => this.setLightState(setLight('scale', params.state)))
+        this.bagno.connect('activate', (params) => this.setLightState(setLight('bagno', params.state)))
+        this.camera.connect('activate', (params) => this.setLightState(setLight('camera', params.state))) 
+        this.armadio.connect('activate', (params) => this.setLightState(setLight('armadio', params.state))) 
+
+        // add items
+        this.menu.addMenuItem(this.ufficio);
+        this.menu.addMenuItem(this.scale);
+        this.menu.addMenuItem(this.bagno);
+        this.menu.addMenuItem(this.camera);
+        this.menu.addMenuItem(this.armadio);
+
+        // prevent menu close
+        this.menu.itemActivated = () => {}
+    }
+
+
+    setLightState(state) {
+        this.ufficio.setToggleState(state.ufficio.state == 1)
+        this.scale.setToggleState(state.scale.state == 1)
+        this.bagno.setToggleState(state.bagno.state == 1)
+        this.camera.setToggleState(state.camera.state == 1)
+        this.armadio.setToggleState(state.armadio.state == 1)
+
+        return true
+    }
+});
+
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+    }
+
+    enable() {
+        this._indicator = new Indicator();
+        Main.panel.addToStatusArea(this._uuid, this._indicator);
+        
+        // get initial state
+        const state = getLightState();
+        this._indicator.setLightState(state);
+
+        Mainloop.timeout_add_seconds(POLL_INTERVAL_SEC, () => {
+            const state = getLightState();
+            this._indicator.setLightState(state);
+            
+            // by returning true the timeout is not canceled
+            return true;
+        });
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
+
+function init(meta) {
+    return new Extension(meta.uuid);
+}
+
