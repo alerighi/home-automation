71cc63cb829d175dc27dafa366dfd2beaefc9232
alerighi <alessandro.righi@iotinga.it>
2021-11-28 15:49:59 +0100

    improve gnome shell extension

--- gnome-shell-extension/extension.js
+++ gnome-shell-extension/extension.js
@@ -1,112 +1,102 @@
-const { GObject, St } = imports.gi;
+const { GObject, St } = imports.gi
 
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Soup = imports.gi.Soup;
-const Mainloop = imports.mainloop;
+const Main = imports.ui.main
+const PanelMenu = imports.ui.panelMenu
+const PopupMenu = imports.ui.popupMenu
+const Soup = imports.gi.Soup
+const Mainloop = imports.mainloop
 
 const POLL_INTERVAL_SEC = 20
 const BASE_URL = 'http://192.168.1.200:8080/light'
+const SOUP_SESSION = new Soup.Session()
+const LIGHTS = ['Ufficio', 'Scale', 'Bagno', 'Camera', 'Armadio']
 
 function setLight(light, value) {
-    const soupSyncSession = new Soup.SessionSync();
-    const message = Soup.Message.new('GET', BASE_URL + '?light=' + light + '&on=' + value);
-    const responseCode = soupSyncSession.send_message(message);
-
-    if (responseCode == 200) {
-        return JSON.parse(message.response_body.data)
+  return new Promise((resolve, reject) => {
+    let args = ''
+    if (value !== undefined && light !== undefined) {
+      args = '?light=' + light + '&on=' + value
     }
+    const message = Soup.Message.new('GET', BASE_URL + args)
+    SOUP_SESSION.queue_message(message, () => {
+      if (message.status_code === Soup.KnownStatusCode.OK) {
+        resolve(JSON.parse(message.response_body.data))
+      } else {
+        reject('!= 200 response code')
+      }
+    })
+  })
 }
-
-
-function getLightState() {
-    const soupSyncSession = new Soup.SessionSync();
-    const message = Soup.Message.new('GET', BASE_URL);
-    const responseCode = soupSyncSession.send_message(message);
-    
-    if (responseCode == 200) {
-        return JSON.parse(message.response_body.data)
-    }
-}
-
 
 const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
+  class Indicator extends PanelMenu.Button {
     _init() {
-        super._init(0.0, 'Home automation');        
+      super._init(0.0, 'Home automation')
 
-        this.add_child(new St.Icon({
-            icon_name: 'dialog-information-symbolic',
-            style_class: 'system-status-icon',
-        }));
+      this.add_child(
+        new St.Icon({
+          icon_name: 'dialog-information-symbolic',
+          style_class: 'system-status-icon',
+        })
+      )
 
-        // create switches
-        this.ufficio = new PopupMenu.PopupSwitchMenuItem('Ufficio', false);
-        this.scale = new PopupMenu.PopupSwitchMenuItem('Scale', false);
-        this.bagno = new PopupMenu.PopupSwitchMenuItem('Bagno', false);
-        this.camera = new PopupMenu.PopupSwitchMenuItem('Camera', false);
-        this.armadio = new PopupMenu.PopupSwitchMenuItem('Armadio', false);
-        
-        // connect signals
-        this.ufficio.connect('activate', (params) => this.setLightState(setLight('ufficio', params.state))) 
-        this.scale.connect('activate', (params) => this.setLightState(setLight('scale', params.state)))
-        this.bagno.connect('activate', (params) => this.setLightState(setLight('bagno', params.state)))
-        this.camera.connect('activate', (params) => this.setLightState(setLight('camera', params.state))) 
-        this.armadio.connect('activate', (params) => this.setLightState(setLight('armadio', params.state))) 
+      // high order functions to set the light state
+      const updateLight = (light) => (params) =>
+        setLight(light, params.state)
+          .then((state) => this.setLightState(state))
+          .catch(console.error)
 
-        // add items
-        this.menu.addMenuItem(this.ufficio);
-        this.menu.addMenuItem(this.scale);
-        this.menu.addMenuItem(this.bagno);
-        this.menu.addMenuItem(this.camera);
-        this.menu.addMenuItem(this.armadio);
+      // create all the menu entries, with a for loop
+      for (const light of LIGHTS) {
+        const key = light.toLowerCase()
+        this[key] = new PopupMenu.PopupSwitchMenuItem(light, false)
+        this[key].connect('activate', updateLight(key))
+        this.menu.addMenuItem(this[key])
+      }
 
-        // prevent menu close
-        this.menu.itemActivated = () => {}
+      // prevent menu close
+      this.menu.itemActivated = () => {}
     }
-
 
     setLightState(state) {
-        this.ufficio.setToggleState(state.ufficio.state == 1)
-        this.scale.setToggleState(state.scale.state == 1)
-        this.bagno.setToggleState(state.bagno.state == 1)
-        this.camera.setToggleState(state.camera.state == 1)
-        this.armadio.setToggleState(state.armadio.state == 1)
-
-        return true
+      for (const light of LIGHTS) {
+        this[light.toLowerCase()].setToggleState(state[light.toLowerCase()].state === 1)
+      }
     }
-});
+  }
+)
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
-    }
+  constructor(uuid) {
+    this._uuid = uuid
+  }
 
-    enable() {
-        this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this._uuid, this._indicator);
-        
-        // get initial state
-        const state = getLightState();
-        this._indicator.setLightState(state);
+  enable() {
+    this._indicator = new Indicator()
+    Main.panel.addToStatusArea(this._uuid, this._indicator)
 
-        Mainloop.timeout_add_seconds(POLL_INTERVAL_SEC, () => {
-            const state = getLightState();
-            this._indicator.setLightState(state);
-            
-            // by returning true the timeout is not canceled
-            return true;
-        });
-    }
+    // get current state
+    setLight()
+      .then((state) => this._indicator.setLightState(state))
+      .catch(console.error)
 
-    disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+    Mainloop.timeout_add_seconds(POLL_INTERVAL_SEC, () => {
+      // get current state
+      setLight()
+        .then((state) => this._indicator.setLightState(state))
+        .catch(console.error)
+
+      // by returning true the timeout is not canceled
+      return true
+    })
+  }
+
+  disable() {
+    this._indicator.destroy()
+    this._indicator = null
+  }
 }
 
 function init(meta) {
-    return new Extension(meta.uuid);
+  return new Extension(meta.uuid)
 }
-
